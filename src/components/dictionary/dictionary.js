ea2597213f5307c0310d7e7fa739c555c7dd328d
Kelby LU <kelby.lu28@gmail.com>
2018-02-25 20:35:17 -0500

    add dictionary and game words

---
+++ src/components/dictionary/dictionary.js
@@ -0,0 +1,14 @@
+import { words } from "./words"
+
+class Dictionary {
+  constructor() {
+    this.dictionary = new Set(words);
+  }
+
+  isValid(word) {
+    return this.dictionary.has(word);
+  }
+
+}
+
+export default Dictionary;
