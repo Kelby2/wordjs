cff5624dc3885c350e749242b595f8c0a5944fdd
Kelby LU <kelby.lu28@gmail.com>
2018-03-02 10:28:01 -0500

    remove comments, complete word

--- src/components/dictionary/word.js
+++ src/components/dictionary/word.js
@@ -1,20 +1,16 @@
-//value
-//shortSubstrings
-//longSubstrings
 import gameWordsLibrary from './game_words';
 import { _getValidSubstrings } from './subwords';
 
 class Word {
 
   constructor() {
-    // this.dictionary = new Dictionary();
     this._generateRandomWord();
     this._getValidSubstrings();
   }
 
   _getValidSubstrings() {
-    this.shortSubs = _getValidSubstrings(this.value, { min: 3, max: 3 } )
-    this.longSubs = _getValidSubstrings(this.value, { min: 4, max: 6 })
+    this.shortSubstrings = _getValidSubstrings(this.value, { min: 3, max: 3 } )
+    this.longSubstrings = _getValidSubstrings(this.value, { min: 4, max: 6 })
   }
 
   _generateRandomWord() {
