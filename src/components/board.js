3c847a661300c1117a63decad52116196a829bf9
Kelby LU <kelby.lu28@gmail.com>
2018-03-03 20:43:08 -0500

    add board methods

--- src/components/board.js
+++ src/components/board.js
@@ -5,28 +5,35 @@ class Board {
   constructor(game) {
     this.game = game;
     this.word = this.game.word;
+    this.populate();
   }
 
   populate() {
     //creates side panels and main game word panel
     this.word.shortSubwords;
     this.word.longSubwords;
-    debugger
+    this.word;
   }
 
-  render() {
-    //render main game_word
-    //render sidelists
+  shuffleLetters() {
+    //shuffle key_word letters
+  }
+
+  updateAnswers() {
+    //update side panels
+  }
+
+  validateInput() {
+    //validate valid key entry (has to be a letter in the word)
+  }
+
+  validateEntry() {
+    //only allow alpha chars, enter, and spacebar
   }
 
   handleInput() {
     //eventListener for input form
   }
-
-  updateBoard() {
-
-  }
-
 
 }
 
