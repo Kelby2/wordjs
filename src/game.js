8c6a2adde9283bf1205658fe624fcda095e2d73c
Kelby LU <kelby.lu28@gmail.com>
2018-03-12 13:00:46 -0400

    added input logic

--- src/game.js
+++ src/game.js
@@ -2,6 +2,7 @@ import Timer from "./components/timer";
 import Score from "./components/score";
 import Word from "./components/dictionary/word";
 import Board from "./components/board";
+import { _charFrequency } from "./components/dictionary/util";
 // import Board from "./components/board";
 
 class Game {
@@ -9,6 +10,7 @@ class Game {
     // this.board = new Board();
     this.gameOver = false;
     this.timer = new Timer(3);
+    this.validateInput = this.validateInput.bind(this);
   }
 
   run() {
@@ -23,17 +25,35 @@ class Game {
 
   handlePlayerEntry() {
     const userInput = document.getElementById("user-input");
-    userInput.addEventListener("keypress", this.validateInput.bind(this));
+    userInput.addEventListener("keypress", this.validateInput);
   }
 
   validateInput() {
-    if (event.keyCode === 32) {
-      event.preventDefault();
-      this.board.shuffleLetters();
+    if (this.isValid(event.key, event.currentTarget)) {
+    } else {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+          this.handleSubmit();
+        }
+        if (event.keyCode === 32) {
+          this.board.shuffleLetters();
+        }
     }
   }
 
+  handleSubmit() {
+    
+  }
 
+  _isValid(letter, inputForm) {
+    //compares letter frequency of word to the input
+    const keyWordCharFrequency = _charFrequency(this.word.value)
+    const inputCharFrequency = _charFrequency(inputForm.value);
+    if (keyWordCharFrequency[letter] === inputCharFrequency[letter]) {
+      return false;
+    }
+    return true;
+  }
 
 
 }
