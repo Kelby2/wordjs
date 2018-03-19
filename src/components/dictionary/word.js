69b3e232384f8f5698124504afe905ea8f853a1a
Kelby LU <kelby.lu28@gmail.com>
2018-03-19 14:58:05 -0400

    change fn name from getValidSubstring to generateSubwords

--- src/components/dictionary/word.js
+++ src/components/dictionary/word.js
@@ -5,12 +5,13 @@ class Word {
 
   constructor() {
     this._generateRandomWord();
-    this._getValidSubstrings();
+    this._generateSubwords();
   }
 
-  _getValidSubstrings() {
+  _generateSubwords() {
     this.shortSubwords = _getValidSubstrings(this.value, { min: 3, max: 3 } );
     this.longSubwords = _getValidSubstrings(this.value, { min: 4, max: 6 });
+    this.allSubwords = [...this.shortSubwords, ...this.longSubwords];
   }
 
   _generateRandomWord() {
