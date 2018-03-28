4765ae1f27b9a4b16931dfeb9af97629330a5ce1
Kelby LU <kelby.lu28@gmail.com>
2018-03-28 01:45:58 -0400

    separate msg for endgame

--- src/components/message.js
+++ src/components/message.js
@@ -6,34 +6,37 @@ class Message {
     this.field = document.getElementById("message");
   }
 
-  alert(entry, type) {
+  display(entry, isAnswer) {
     let message;
     const score = calculateWordScore(entry);
-    const color = (type === "valid") ? "green" : "#990016";
+    const color = (isAnswer === true) ? "#008000" : "#990016";
     const word = `<span class="strong" style="color:${color}">
     ${entry}</span>`;
 
-    switch (type) {
-      case "valid":
+    switch (isAnswer) {
+      case true:
         message = `<span class="strong" style="color:${color}">
         ${score}</span> points for ${word}`;
         break;
       case "duplicate":
         message = `${word} has already been played`;
         break;
-      case "invalid":
+      case false:
         message = `${word}<br>does not exist in the dictionary`;
         break;
-      case "end":
-        message = `You got ${word}% of the words this round!`;
-        break;
     }
+    this.field.innerHTML = message;
+  }
 
+  conclude(percentage) {
+    const color = (percentage > 49) ? "#008000" : "#990016";
+    const message = `You got <span class="strong" style="color:${color}">
+    ${percentage}%</span> of the words!`;
     this.field.innerHTML = message;
   }
 
   alertMin() {
-    this.field.innerHTML = "Please enter a minimum of 3 letters";
+    this.field.innerHTML = "Please enter at least 3 letters";
   }
 
   clear() {
