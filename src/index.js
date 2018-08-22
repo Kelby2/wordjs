cf71dfbbf23d0569562c1f073ef943ff8bfef989
Kelby Lu <kelby.lu28@gmail.com>
2018-08-22 16:08:18 -0400

    Add firebase initialization on app load

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as firebase from 'firebase/app';
 import GameHandler from "./game";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,6 +12,16 @@ document.addEventListener("DOMContentLoaded", () => {
       game.beginRound();
     });
   };
+
+  const config = {
+    apiKey: 'AIzaSyAbTuJaoP1ZaUlBzkGbowEBN6IdK5SPsNc',
+    authDomain: 'wordjs-80f4a.firebaseapp.com',
+    databaseURL: 'https://wordjs-80f4a.firebaseio.com',
+    projectId: 'wordjs-80f4a',
+    storageBucket: 'wordjs-80f4a.appspot.com',
+    messagingSenderId: '726517595431'
+  };
+  firebase.initializeApp(config);
 
   const instructions = document.getElementById("ins");
   const insToggler = document.getElementById("ins-btn");
